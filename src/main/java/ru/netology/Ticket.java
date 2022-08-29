f4eb71a225c27fb01982d0abeb4edaf6ae028fee
Василий Скоробогатов <vaskorobogatov@yandex.ru>
2022-08-30 01:05:58 +0300

    TicketFinder with 100% coverage

---
+++ src/main/java/ru/netology/Ticket.java
@@ -0,0 +1,40 @@
+package ru.netology;
+
+public class Ticket implements Comparable<Ticket> {
+  int id;
+  int price;
+  String from;
+  String to;
+  int travelTime;
+
+  public Ticket(int id, int price, String from, String to, int travelTime) {
+    this.id = id;
+    this.price = price;
+    this.from = from;
+    this.to = to;
+    this.travelTime = travelTime;
+  }
+
+  @Override
+  public int compareTo(Ticket o) {
+    return Integer.compare(this.price, o.price);
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getDepartureAirport() {
+    return from;
+  }
+
+  public String getArrivalAirport() {
+    return to;
+  }
+
+  public boolean matches(String from, String to) {
+    if (getDepartureAirport().contains(from)) {
+      return getArrivalAirport().contains(to);
+    } else return false;
+  }
+}
---
+++ src/test/java/ru/netology/ManagerTest.java
@@ -0,0 +1,53 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ManagerTest {
+  TicketRepository repository = new TicketRepository();
+  TicketManager manager = new TicketManager(repository);
+
+  Ticket ticket1 = new Ticket(1, 10_000, "DME", "KZN", 95);
+  Ticket ticket2 = new Ticket(2, 9_500, "SVO", "KZN", 90);
+  Ticket ticket3 = new Ticket(3, 5_000, "DME", "KZN", 190);
+  Ticket ticket4 = new Ticket(4, 5_000, "SVO", "KZN", 180);
+  Ticket ticket5 = new Ticket(5, 15_000, "DME", "TJM", 180);
+  Ticket ticket6 = new Ticket(6, 9_500, "SVO", "KZN", 175);
+  Ticket ticket7 = new Ticket(7, 20_000, "KZN", "TJM", 210);
+  Ticket ticket8 = new Ticket(8, 20_000, "TJM", "KZN", 210);
+  Ticket ticket9 = new Ticket(9, 10_000, "KZN", "DME", 95);
+  Ticket ticket10 = new Ticket(10, 9_500, "KZN", "SVO", 90);
+  Ticket ticket11 = new Ticket(11, 2_500, "SVO", "KZN", 380);
+
+  @BeforeEach
+  public void addTickets() {
+    repository.addTicket(ticket1);
+    repository.addTicket(ticket2);
+    repository.addTicket(ticket3);
+    repository.addTicket(ticket4);
+    repository.addTicket(ticket5);
+    repository.addTicket(ticket6);
+    repository.addTicket(ticket7);
+    repository.addTicket(ticket8);
+    repository.addTicket(ticket9);
+    repository.addTicket(ticket10);
+    repository.addTicket(ticket11);
+  }
+
+  @Test
+  public void testFindTickets() {
+
+    Ticket[] expectedArray1 = {ticket3, ticket1};
+    Assertions.assertArrayEquals(expectedArray1, manager.findAll("DME", "KZN"));
+
+    Ticket[] expectedArray2 = {ticket11, ticket4, ticket2, ticket6};
+    Assertions.assertArrayEquals(expectedArray2, manager.findAll("SVO", "KZN"));
+
+    Ticket[] expectedArray3 = {};
+    Assertions.assertArrayEquals(expectedArray3, manager.findAll("TJM", "DME"));
+
+    Ticket[] expectedArray4 = {ticket8};
+    Assertions.assertArrayEquals(expectedArray4, manager.findAll("TJM", "KZN"));
+  }
+}
